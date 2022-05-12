809b211cd1472b4940f0a8ff01bff7db1febfbdf
slavks <exeherrera96@gmail.com>
2022-05-12 14:29:32 -0300

    first commit

---
+++ src/componentes/inicio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Inicio() {
+    return (
+        <div className="nuevo-div">
+            <h1 className="titulo">
+            No conoces el poder del Lado Oscuro..
+            </h1>
+            <img src="http://todovector.com/vector/cine-y-celebridades/star-wars/darth-vader/3.png"/>
+        </div>
+    )
+}
